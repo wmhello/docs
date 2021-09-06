3028486b5389e406043f2830de08ecba56876568
“wmhello <871228582@qq.com>
2021-09-06 20:06:49 +0800

    编码

--- scripts/deploy-gh.sh
+++ scripts/deploy-gh.sh
@@ -4,10 +4,10 @@
 set -e
 
 # 生成静态文件
-npm run build
+yarn build
 
 # 进入生成的文件夹
-cd vuepress-hope/.vuepress/dist
+cd docs
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -23,6 +23,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 # 把上面的 <USERNAME> 换成你自己的 Github 用户名，<REPO> 换成仓库名，比如我这里就是：
-git push -f git@github.com:wtyqer/blog.git master:gh-pages
+git push -f git@github.com:wmhello/docs.git master:gh-pages
 
 cd -h
